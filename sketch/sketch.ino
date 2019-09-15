56fe168304204b919b663e58d9a9dfd4b2d2ba1a
Tommaso Morganti <tommaso.morganti01@gmail.com>
2019-09-15 20:23:18 +0200

    fastled

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -13,15 +13,12 @@ WiFiClient wclient;
 PubSubClient client = PubSubClient(wclient);
 
 // LED configurations
-#include <Adafruit_NeoPixel.h>
-#ifndef D4
-#define D4 2
-#endif
+#include <FastLED.h>
 
 #define PIXELAMOUNT 18
-#define LEDPIN D4
+#define LEDPIN 4
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXELAMOUNT, LEDPIN, NEO_RGB + NEO_KHZ400);
+CRGBArray<PIXELAMOUNT> leds;
 
 #define STEPS 20
 #define TRANSITION_TIME 500
@@ -30,30 +27,46 @@ int missingSteps = 0;
 float cR = 0, tR = 0;
 float cG = 0, tG = 0;
 float cB = 0, tB = 0;
+int brightness = 0;
+String mode = "perlin";
 
 /* Code */
 // callback for mqtt connection
 void callback(char *topic, byte *payload, unsigned int length)
 {
     String inTopic = String(topic);
-    char pl[length];
+    char pla[length];
     for (int i = 0; i < length; i++)
     {
-        pl[i] = payload[i];
+        pla[i] = payload[i];
     }
-    int value = String(pl).toInt();
-    tR = (value >> 16) & 0xff;
-    tG = (value >> 8) & 0xff;
-    tB = value & 0xff;
+
+    String pl = String(pla);
+
+    if (inTopic == "color")
+    {
+        int value = pl.toInt();
+        tR = (value >> 16) & 0xff;
+        tG = (value >> 8) & 0xff;
+        tB = value & 0xff;
+    }
+    else if (inTopic == "brightness")
+    {
+        brightness = pl.toInt();
+    }
+    else if (inTopic == "mode")
+    {
+        mode = pl;
+    }
+
     missingSteps = STEPS;
 }
 
 void setup()
 {
-    strip.begin();
-    strip.clear();
-    strip.show();
-    WiFi.begin();
+    FastLED.addLeds<WS2812, LEDPIN, RBG>(leds, PIXELAMOUNT).setCorrection(TypicalLEDStrip);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
@@ -69,37 +82,61 @@ void loop()
     if (WiFi.status() == WL_CONNECTED)
     {
         ArduinoOTA.handle();
-
-        if (!client.connected())
-        {
-            if (client.connect("ESP8266: ledstrip"))
-            {
-                client.subscribe("color");
-            }
-        }
-        else
-        {
-            client.loop();
-            if (missingSteps > 0)
-            {
-                updateColor();
-                delay(TRANSITION_TIME / STEPS);
-            }
-        }
+        // if (!client.connected())
+        // {
+        //     if (client.connect("ESP8266: ledstrip"))
+        //     {
+        //         client.subscribe("color");
+        //         Serial.println("Subscibing to MQTT!");
+        //     }
+        // }
+        // else
+        // {
+        //     client.loop();
+        //     Serial.println("MQtt!!");
+        // }
     }
+    else
+        WiFi.reconnect();
+
+    if (mode == "fixed")
+        fixed();
+    else if (mode == "rainbow")
+        fixed();
+    else if (mode == "perlin")
+        perlin();
+
+    delay(33);
 }
 
-void updateColor()
+void fixed()
 {
-    cR += (tR - cR) / missingSteps;
-    cG += (tG - cG) / missingSteps;
-    cB += (tB - cB) / missingSteps;
+    // cR += (tR - cR) / missingSteps;
+    // cG += (tG - cG) / missingSteps;
+    // cB += (tB - cB) / missingSteps;
 
-    for (int i = 0; i < strip.numPixels(); i++)
+    // for (int i = 0; i < strip.numPixels(); i++)
+    // {
+    //     strip.setPixelColor(i, strip.gamma32(strip.Color(cR, cG, cB)));
+    // }
+
+    // strip.show();
+    // missingSteps--;
+}
+
+static uint16_t perlinOffset = 0;
+void perlin()
+{
+    for (int i = 0; i < PIXELAMOUNT; i++)
     {
-        strip.setPixelColor(i, strip.gamma32(strip.Color(cR, cG, cB)));
+        int bri = 220 + inoise8_raw(i * 100, perlinOffset * 10) * 40 / 70;
+        if (bri > 255)
+            bri = 255;
+        if (bri < 0)
+            bri = 0;
+
+        leds[i] = CHSV(300, 247, bri);
     }
-    
-    strip.show();
-    missingSteps--;
+    FastLED.show();
+    perlinOffset++;
 }
